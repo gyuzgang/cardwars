c293b10b0ef9b81973f08e6652d6cddacaa50c45
yurago <www.yurag@gmail.com>
2022-03-25 23:33:24 +0300

    init

---
+++ src/main/java/com/gyteam/cardwars/CardwarsApplication.java
@@ -0,0 +1,13 @@
+package com.gyteam.cardwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardwarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyteam/cardwars/CardwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyteam.cardwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
